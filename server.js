fe736cf56fbbf8058c597a6f5195b5f938657b82
johannes <johannes.hassenstein@yesdevs.com>
2023-04-28 12:07:24 +0200

    updated names for snk

--- server.js
+++ server.js
@@ -138,7 +138,7 @@ const poi = {
           "#a8ffca"
         ]
       ],
-      artists:"Yifan He, Nuka Nayu, Axe Binondo, Chaney Diao, Zhuyang Liu, Nitesh Tailor, Nikki C",
+      artists:"a gathering of Chaney Diao, Kash & Germ, Axe Binondo, Zhuyang Liu, Nitesh Tailor, Song Han, Chia Amisola, Jiawei Zheng, angeldust and Nikki Caranzo. Curated and web design by yifan he",
       date:"28.04.23",
       description:"portals, props, and the false promises of (other) worlds<br><br>a fabulated open-doc/ html/ possible live stream performances or game play (expression of interest: Zhuyang)<br><br>Swords and kisses is an exhibition that prioritises transvisions of the non-linear time space wraps around queer skins and hairs. Interested in the portals, props, and the false promises of (the other) worlds, queer digital artists, sound artists, writers who write about games, and performers will be weaving their works together in the simple format of a website. We lay trusts in unimpressive tech as the fabric to communities of fantasy. And we attempt to use the queer technology of fantasy– world building, body (avatars) building, and storytelling– to suggest fabulous ways to be with each other (with care, nightmares, and fan-fictions).",
       link: "https://he-yifan.com/swordsnkisses.html"
