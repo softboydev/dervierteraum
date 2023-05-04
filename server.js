1f0fb86a574daa515821a9896353bf26beec24fc
johannes <johannes.hassenstein@yesdevs.com>
2023-05-04 14:16:28 +0200

    update for noah brosh

--- server.js
+++ server.js
@@ -108,6 +108,42 @@ const poi = {
       link: "http://heikowommelsdorf.de/tv-phase-im-4ten-raum"
     }
   },
+  "6;24": {
+    name: "Identity Crisis",
+    id: "identity-crisis",
+    x: 6,
+    y: 24,
+    poi: true,
+    exhibit: true,
+    state: {
+      plan: [
+        [
+          5,
+          2,
+          3,
+          5,
+          4,
+          4,
+          4,
+          5
+        ],
+        [
+          "#ffffff",
+          "#ffffff",
+          "#2204b9",
+          "#2204b9",
+          "#8bf58a",
+          "#8cf58a",
+          "#8cf58a",
+          "#2204b9"
+        ]
+      ],
+      artists:"Noa Brosh ",
+      date:"05.05.23",
+      description:"In today's era, even the most basic thing we have - our identity - is no longer certain<br><br>A few months ago, when I was living in Hamburg, my identity was stolen.<br><br>For the following 24 hours, I was distraught. I felt that the most basic thing I have, my identity, was being stripped away from me.<br><br>While I was deeply disturbed by the thought of someone else claiming to be me and doing things under my name, I was also curious as to how it could happen.<br><br>I decided to start researching.<br><br>The project Identity Crisis is the result of that research. It is divided into 3 parts, each is a question that has accompanied me throughout the ordeal.<br><br>1. Is it possible to produce something that sounds exactly like me?<br>2. Is it possible to produce something that looks, and sounds like me?<br>3. How much information about me is accessible at any given time?<br><br>This project is done using artificial intelligence, the character I created sounds like me and looks like me, but none of this is real.",
+      link: "https://identitycrisis.dervierteraum.org"
+    }
+  },
   "20;6": {
     name: "Swords and Kisses",
     id: "swords-and-kisses",
