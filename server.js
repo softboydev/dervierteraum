933824936dbc9c22802702a55eba73ccaa4179ee
johanneshassenstein <johannes.hassenstein@protonmail.com>
2023-11-14 18:49:08 +0100

    added disturba

--- server.js
+++ server.js
@@ -302,6 +302,42 @@ const poi = {
       description:"General singt vom Frühling - Eine VR-Traum-Installation<br><br>'Der befehlende General der russischen Armee sagte zu den aufgestellten Schülern: Bis hierher müßt ihr euch aufstellen, davor hatte er sich mit dem Helm im Rock einer Madga verfangen, bevor er verschwand. dann sang einer vom Frühling. bis dahin sangen die Schüler, als er verschwunden war, und irgend jemand sang was vom Frühling.'<br><br>Bernstein, Sigrid: Edelsteinbäume – ein Traumbuch, edition fischer, 2019, Frankfurt, S. 18<br><br>Wer folgende Fragen während der Ausstellung zu dem Video beantwortet, kann diese Antworten dem Künstler erzählen, und einen signierten Fine-Art-Print von „Mir Minsk' dafür erhalten:<br>Nicht alle Soldaten machen beim Spießrutenlauf mit – wie viele küssen stattdessen?<br>Wie viele Uniform-Knöpfe haben die russischen Soldaten im Traum?<br>Welche Verbindungen gibt es zu dem Bild „Mir Minsk' in der Ausstellung?<br><br>Antworten bitte an jendrik@gmx.de",
       link: "https://www.traumdatenbank.de/Wordpress/2023/08/19/s-b-1994/"
     }
+  },
+  "riconoscersifragildisturba": {
+    name: "riconoscersi attimi sospesi di fragilità in divenire disturba",
+    id: "riconoscersifragildisturba",
+    x: 28,
+    y: 7,
+    poi: true,
+    exhibit: true,
+    state: {
+      plan: [
+        [
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4,
+          4
+        ],
+        [
+          "#ffffff",
+          "#ffffff",
+          "#ffffff",
+          "#ffffff",
+          "#ffffff",
+          "#ffffff",
+          "#ffffff",
+          "#ffffff"
+        ]
+      ],
+      artists:"Riccardo Androni",
+      date:"17.11.2023",
+      description:"riconoscersi attimi sospesi di fragilità in divenire disturba tells a visual and auditive story, mostly metaphorical, which uses and combines various media, as well as supports, in an attempt to tear the veil of the obvious in order to probe the inner abysses of the human being and investigate its fragility. It is an invitation to embark on a journey, it is a way to go throughout the existential labyrinth. Its purpose is to expose and disarm the viewer which, getting lost, can rediscover his own negative capability: stay in uncertainties, mysteries and doubts without being impatient to arrive at facts and reasons.<br><br>It is a rare opportunity to loosen the curtain, to go beyond the declared world, to travel through unexplored universes which could awaken asleep questions. May it is possible to find some answers or truths if the limit is inhabited without reserve. It is the beginning of a journey, it is an ode to vulnerability. It’s a hope for our own awareness in order to be able to act in the future and rise again.",
+      link: "https://disturba.dervierteraum.org"
+    }
   }
 }
 let PLAYERS = {}
