c4b639fb00daf1e9bf530f4ee9beff3d202cd774
johanneshassenstein <johannes.hassenstein@protonmail.com>
2023-12-04 10:08:06 +0100

    added softmaps

--- server.js
+++ server.js
@@ -338,6 +338,42 @@ const poi = {
       description:"riconoscersi attimi sospesi di fragilità in divenire disturba tells a visual and auditive story, mostly metaphorical, which uses and combines various media, as well as supports, in an attempt to tear the veil of the obvious in order to probe the inner abysses of the human being and investigate its fragility. It is an invitation to embark on a journey, it is a way to go throughout the existential labyrinth. Its purpose is to expose and disarm the viewer which, getting lost, can rediscover his own negative capability: stay in uncertainties, mysteries and doubts without being impatient to arrive at facts and reasons.<br><br>It is a rare opportunity to loosen the curtain, to go beyond the declared world, to travel through unexplored universes which could awaken asleep questions. May it is possible to find some answers or truths if the limit is inhabited without reserve. It is the beginning of a journey, it is an ode to vulnerability. It’s a hope for our own awareness in order to be able to act in the future and rise again.",
       link: "https://disturba.dervierteraum.org"
     }
+  },
+  "softmaps": {
+    name: "Softmaps",
+    id: "softmaps",
+    x: 6,
+    y: 32,
+    poi: true,
+    exhibit: true,
+    state: {
+      plan: [
+        [
+          1,
+          3,
+          1,
+          0,
+          5,
+          3,
+          5,
+          1
+        ],
+        [
+          "#ffffff",
+          "#0000ff",
+          "#00ffff",
+          "#00ffff",
+          "#00ff00",
+          "#0000ff",
+          "#0000ff",
+          "#00ffff"
+        ]
+      ],
+      artists:"Lily McCraith, Pheobe Riley Law",
+      date:"03.12.2023",
+      description:"Soft maps attempts to re-build tools for navigation and orientation by looking at mapping through an alternative lens. Here, a swimming pool is our working model, and the act of cleaning the pool becomes an algorithm for mapping. We explore how tools and presence shape maps, forming a multi-perspective map & score that questions conventional cartographic practices. It matters which maps map maps.",
+      link: "https://softmaps.dervierteraum.org"
+    }
   }
 }
 let PLAYERS = {}
